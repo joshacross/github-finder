233b9f48a9f702939ee828fa4d5525055ee5b43d
joshacross <joshuaalexandercross@gmail.com>
2022-08-22 06:23:24 -0500

    initalized app

---
+++ github-finder/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+     return (
+          <div className='App'>
+               <header className='App-header bg-purple-500'>
+                    <h1 className='text-3xl text-black'>Hello World</h1>
+                    <button className='btn'>This is a button</button>
+               </header>
+          </div>
+     );
+}
+
+export default App;
